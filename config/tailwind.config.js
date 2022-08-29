7b34711351ae3f612d2b8862c2a9a809895f40b3
Miguelanggelo Sumoza <miguelanggelo21@gmail.com>
2022-08-28 23:32:50 -0400

    Idk

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,6 +19,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
 
-    require('tw-elements/dist/plugin'),
+    require('tw-elements'),
   ],
 };
