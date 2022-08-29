1ceb61c3397e2ede60088795633651c6f4f6e127
Miguelanggelo Sumoza <miguelanggelo21@gmail.com>
2022-08-29 12:20:29 -0400

    node_modules

--- app/javascript/controllers/bitcoin_blocks_controller.js
+++ app/javascript/controllers/bitcoin_blocks_controller.js
@@ -71,7 +71,7 @@ export default class extends Controller {
         `
         <div class="alert bg-red-100 mt-6 rounded-lg py-5 px-6  text-base text-red-700 inline-flex items-center w-full alert-dismissible fade show" role="alert">
           ${message}
-          <button type="button" class="btn-close box-content w-4 h-4 p-1 ml-auto text-red-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-red-900 hover:opacity-75 hover:no-underline" data-bs-dismiss="alert" aria-label="Close">X</button>
+          <button type="button" class="btn-close box-content w-4 h-4 p-1 ml-auto text-red-900 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-red-900 hover:opacity-75 hover:no-underline" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
       `
       );
